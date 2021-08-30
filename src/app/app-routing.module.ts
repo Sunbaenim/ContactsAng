1b4d46adfa8c3acbdbd5fb51e80efc57aa0df979
Dylan Lootens <lootens.dylan@edu.technobel.be>
2021-08-30 16:53:06 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'contact', loadChildren: () => import('./features/contact/contact.module').then(m => m.ContactModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
